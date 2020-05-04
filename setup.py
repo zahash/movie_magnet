0cb10278465c8f76e1aa13cecaefe8250d54f952
Zahash Baig <zahash.z@gmail.com>
2020-05-04 12:47:45 +0530

    initialize setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+# python3 setup.py sdist bdist_wheel
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="movie_magnet",
+    version="0.0.1",
+    description="this python tool gets torrent magnet like to any movie",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zahash/movie_magnet",
+    author="zahash",
+    author_email="zahash.z@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["movie_magnet"],
+    include_package_data=True,
+    install_requires=["bs4", "requests", "pandas", "tabulate", "termcolor", "pyfiglet"],
+    entry_points={"console_scripts": ["movie_magnet=movie_magnet.__main__:main",]},
+)
