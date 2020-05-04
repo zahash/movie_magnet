191f0ec0840445f75453359408ee441f8176483f
Zahash Baig <zahash.z@gmail.com>
2020-05-04 13:13:58 +0530

    bumped patch version from 0.0.1 to 0.0.2

--- movie_magnet/__init__.py
+++ movie_magnet/__init__.py
@@ -1,3 +1,3 @@
 # bumpversion --current-version <__version__> (major|minor|patch) setup.py movie_magnet/__init__.py
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="movie_magnet",
-    version="0.0.1",
+    version="0.0.2",
     description="Python tool to get magnet links to any movie",
     long_description=README,
     long_description_content_type="text/markdown",
